915de47d1b510aa625e037e7a3fb1f742da32656
dariusz.zdybski <dariusz.zdybski@uj.edu.pl>
2017-12-07 09:05:22 +0100

    modyfing test, pom, adding driver

--- src/test/java/WebsiteTest.java
+++ src/test/java/WebsiteTest.java
@@ -1,8 +1,12 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
 
 /**
  * Created by Dariusz Zdybski on 2017-12-06.
@@ -16,19 +20,33 @@ public class WebsiteTest {
 
     @Before
     public void setUp(){
+
+
+        final File file = new File("src/test/resources/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+
         driver = new ChromeDriver();
         searchPhrase = "test”";
-        String jobLocation = "Krakow office";
-        String url = "https://akamaijobs.referrals.selectminds.com/";
+         jobLocation = "Krakow office";
+         url = "https://akamaijobs.referrals.selectminds.com/";
     }
 
     @Test
     public void testSearch(){
 
+        driver.get(url);
+        WebElement searchForm = driver.findElement(By.name("keyword"));
+        WebElement searchLocation = driver.findElement(By.className("jLocPlaceholder"));
+        WebElement searchButton = driver.findElement(By.className("btn_text"));
+
+        searchForm.clear();
+        searchForm.sendKeys("test");
+        searchLocation.sendKeys("Krakow, Poland");
+        searchButton.click();
     }
 
     @After
     public void tearDown(){
-
+        driver.quit();
     }
 }
